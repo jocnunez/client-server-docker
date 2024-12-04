2b6fb407e1fee0dc644d739a453898fdb0bf990b
Your Name <you@example.com>
2024-12-04 13:41:25 +0100

    04 dic

---
+++ AoC/day01/main.py
@@ -0,0 +1,6 @@
+FILE_NAME = 'input.txt'
+
+with open(FILE_NAME) as file:
+    lines = file.readlines()
+    for line in lines:
+        frase = "esto es una frase"
--- api/src/main.py
+++ api/src/main.py
@@ -1,1 +1,26 @@
-print("Hello python!!!")
+print("Initializing ToDos API!")
+
+from typing import Union
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+@app.get("/todos")
+def read_item():
+    #TODO: consultar en base de datos mi lista de todos
+    todos = [
+                {"text": "tarea1", "check": "false"},
+                {"text": "tarea2", "check": "false"},
+                {"text": "tarea3", "check": "false"}
+            ]
+    return todos
